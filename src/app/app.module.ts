7bbb6903c849a2dbc2fd504ecf591aecf304a584
brisarosatti <brisa.rosatti@gmail.com>
2023-07-26 15:10:21 -0300

    add new components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { AppRoutingModule } from './modules/app-routing.module';
-
-import { SvgComponentsModule } from './modules/svg-components.module';
 import { HttpClientModule } from '@angular/common/http';
 
+import { AppRoutingModule } from './modules/app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
-import { HeaderComponent } from './components/header/header.component';
+
 import { CardBoxComponent } from './components/card-box/card-box.component';
-import { CarouselItemsComponent } from './components/carousel-items/carousel-items.component';
+import { HeaderComponent } from './components/header/header.component';
 import { AmountBoxComponent } from './components/amount-box/amount-box.component';
+import { CarouselItemsComponent } from './components/carousel-items/carousel-items.component';
+
+import { SvgComponentsModule } from './modules/svg-components.module';
+
 
 @NgModule({
 	declarations: [
 		AppComponent,
-  HomeComponent,
-  HeaderComponent,
-  CardBoxComponent,
-  CarouselItemsComponent,
-  AmountBoxComponent,
+		HomeComponent,
+		CardBoxComponent,
+		HeaderComponent,
+		AmountBoxComponent,
+		CarouselItemsComponent
 	],
 	imports: [
 		BrowserModule,
